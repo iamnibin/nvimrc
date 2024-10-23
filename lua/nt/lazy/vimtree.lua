eb87db0d0bc4f49f076d2f808c0a56b3b02f93bb
nibin <nibin@telaverge.com>
2024-10-23 11:01:59 +0000

    added vimtree

--- lua/nt/lazy/vimtree.lua
+++ lua/nt/lazy/vimtree.lua
@@ -1,19 +1,13 @@
-local function on_attach(bufnr)
-  local api = require "nvim-tree.api"
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
-
-  vim.keymap.set('n', '<leader>fl', api.tree.change_root_to_parent,        opts('Up'))
-end
-
--- pass to setup along with your other options
 return {
-  "nvim-tree/nvim-tree.lua",
-  on_attach = on_attach,
-  ---
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("nvim-tree").setup {}
+    end,
 }
+
