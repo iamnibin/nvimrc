7cc07da48728ef3e2e25acfa66bc93bf6fe43149
nibin <nibinthomasissac@gmail.com>
2024-11-26 06:38:15 +0530

    Adding Autosave

--- lua/nt/lazy/vimtree.lua
+++ lua/nt/lazy/vimtree.lua
@@ -6,6 +6,16 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
+    on_attach = function(bufnr)
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "<leader>el", ":NvimTreeToggle")
+    end,
     config = function()
         require("nvim-tree").setup {}
     end,
