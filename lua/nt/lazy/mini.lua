968ea5fe6f353db437faaa062b3cf696fd5afbb9
yongconnect-devops <devops@yongconnect.com>
2025-01-23 09:25:54 +0530

    adding mini and tmux navigator

---
+++ lua/nt/lazy/mini.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'echasnovski/mini.surround',
+    config = function()
+      require('mini.surround').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.ai',
+    config = function()
+      require('mini.ai').setup()
+    end,
+  },
+}
